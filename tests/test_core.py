83e34b6bc9f8bab8868be1dcf18ee9cbf5b6b2d0
Benjamin Theunissen <ben@shaped.ai>
2023-06-13 14:16:08 -0400

    Test native connection type

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,19 +9,42 @@ from singer_sdk.testing import get_target_test_class
 
 from target_clickhouse.target import TargetClickhouse
 
-SAMPLE_CONFIG: dict[str, t.Any] = {
+SAMPLE_HTTP_CONFIG: dict[str, t.Any] = {
     "sqlalchemy_url": "clickhouse+http://default:@localhost:18123",
 }
 
+SAMPLE_NATIVE_CONFIG: dict[str, t.Any] = {
+    "sqlalchemy_url": "clickhouse+native://default:@localhost:18123",
+}
+
 # Run standard built-in target tests from the SDK:
-StandardTargetTests = get_target_test_class(
+StandardHTTPTargetTests = get_target_test_class(
     target_class=TargetClickhouse,
-    config=SAMPLE_CONFIG,
+    config=SAMPLE_HTTP_CONFIG,
 )
 
+StandardNativeTargetTests = get_target_test_class(
+    target_class=TargetClickhouse,
+    config=SAMPLE_HTTP_CONFIG,
+)
 
-class TestTargetClickhouse(StandardTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
-    """Standard Target Tests."""
+class TestTargetClickhouseHttp(StandardHTTPTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
+    """Standard HTTP Connection Target Tests."""
+
+    @pytest.fixture(scope="class")
+    def resource(self):  # noqa: ANN201
+        """Generic external resource.
+
+        This fixture is useful for setup and teardown of external resources,
+        such output folders, tables, buckets etc. for use during testing.
+
+        Example usage can be found in the SDK samples test suite:
+        https://github.com/meltano/sdk/tree/main/tests/samples
+        """
+        return "resource"
+
+class TestTargetClickhouseNative(StandardNativeTargetTests):  # type: ignore[misc, valid-type]  # noqa: E501
+    """Standard Native Connection Target Tests."""
 
     @pytest.fixture(scope="class")
     def resource(self):  # noqa: ANN201
