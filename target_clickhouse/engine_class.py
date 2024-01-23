68a69e1d069032f92bbcb35d6c74369aebbe3917
Alik Kurdyukov <alik@kurdyukov.com>
2024-01-23 17:24:53 +0400

    make linter happy

--- target_clickhouse/engine_class.py
+++ target_clickhouse/engine_class.py
@@ -1,6 +1,6 @@
 from enum import Enum
-from typing import List, Optional
 from string import Template
+from typing import List, Optional
 
 from clickhouse_sqlalchemy import engines
 from sqlalchemy import func
