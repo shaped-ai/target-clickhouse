1c79aa057b53c99b4dba067a27c05a9a4b2e6e7c
Jesus Manuel Gallego Romero <jmgallego@idealista.com>
2024-02-23 12:45:54 +0100

    added test for all types column

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,14 +2,18 @@
 
 from __future__ import annotations
 
+import os
 import typing as t
+from pathlib import Path
 
-from singer_sdk.testing import get_target_test_class
+from singer_sdk.testing import get_target_test_class, suites
+from singer_sdk.testing.suites import TestSuite
+from singer_sdk.testing.templates import TargetFileTestTemplate
 
 from target_clickhouse.target import TargetClickhouse
 
 TEST_CONFIG: dict[str, t.Any] = {
-    "sqlalchemy_url": "clickhouse+http://default:@localhost:8123",
+    "sqlalchemy_url": "clickhouse+http://default:@localhost:8123"
 }
 
 TEST_CONFIG_SPREAD: dict[str, t.Any] = {
@@ -35,12 +39,31 @@ TEST_CONFIG_NATIVE: dict[str, t.Any] = {
     "verify": False,
 }
 
+class TargetAllTypesTest(TargetFileTestTemplate):
+    """Test Target handles array data."""
+    name = "all_types"
+
+    @property
+    def singer_filepath(self) -> Path:
+        """Get path to singer JSONL formatted messages file.
+
+        Files will be sourced from `./target_test_streams/<test name>.singer`.
+
+        Returns:
+            The expected Path to this tests singer file.
+        """
+        return os.path.abspath(os.path.join(os.path.join(os.path.join(__file__, os.pardir), "resources"), f"{self.name}.singer"))
+
+custom_test_key_properties = suites.TestSuite(
+    kind="target",
+    tests=[TargetAllTypesTest]
+)
 # Run standard built-in target tests from the SDK:
 StandardTargetTests = get_target_test_class(
     target_class=TargetClickhouse,
     config=TEST_CONFIG,
+    custom_suites=[custom_test_key_properties],
 )
-
 
 
 class TestStandardTargetClickhouse(StandardTargetTests):  # type: ignore[misc, valid-type]
@@ -52,6 +75,6 @@ SpreadTargetTests = get_target_test_class(
     config=TEST_CONFIG_SPREAD,
 )
 
-
 class TestSpreadTargetClickhouse(SpreadTargetTests):  # type: ignore[misc, valid-type]
     """Standard Target Tests."""
+
